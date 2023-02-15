941ab1265af12381ca009259a50057fe385606bc
harrison531 <harrisongardner531@gmail.com>
2023-02-15 10:31:09 -0700

    Movement, and camera look
    Currently an issue with the mouse axis deltas where they return a massive number. I have it setup so that it will prevent it from making the player look in some random direction but it is causing some frame hiccups. Will have to look into it more later on.

--- Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/CameraLook.cs
+++ Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/CameraLook.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
-    public float lookSensitivity = 15f;
     private GameObject cam;
+    public float camHeight = 0.55f;
+    public float lookSensitivity = 15f;
     private Vector2 rotation = Vector2.zero;
+
+
+    private Vector2 lastFrameRotation = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +23,36 @@ public class CameraLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //TODO: Clamp rotation
-        //TODO: Figure out why it's jittery when rotating
-        rotation += new Vector2(-Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime, Input.GetAxis("Mouse X") * lookSensitivity * Time.deltaTime);
-        cam.transform.localEulerAngles = new Vector3(rotation.x, 0, 0);
-        transform.eulerAngles = new Vector3(0, rotation.y, 0);
+        Vector2 mouseInputs = new Vector2(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"));
+
+        rotation += new Vector2(mouseInputs.x * lookSensitivity * Time.deltaTime, mouseInputs.y * lookSensitivity * Time.deltaTime);
+
+        //clamp up-down rotation
+        rotation.x = Mathf.Clamp(rotation.x, -70f, 70f);
+
+        Vector2 rotationDelta = lastFrameRotation - rotation;
+        
+
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y + camHeight, transform.position.z);
+
+        // Apply camera rotation
+        // make sure the change in rotation isn't too great from the last frame,
+        // there's an issue where Input.GetAxisRaw with the mouse deltas will return a massive number
+        if (Mathf.Abs(rotationDelta.x) < 75f && Mathf.Abs(rotationDelta.y) < 75f)
+        {
+            cam.transform.eulerAngles = new Vector3(rotation.x, rotation.y, 0);
+            lastFrameRotation = rotation;
+        }
+        else
+        {
+            rotation = lastFrameRotation;
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Apply player rotation
+        transform.eulerAngles = new Vector3(0, cam.transform.eulerAngles.y, 0);
     }
 }
--- Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerMovement.cs
+++ Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerMovement.cs
@@ -7,8 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody rb;
-    //TODO: Rename this variable tomorrow when I can think, this is not a good name for it
-    private Vector3 movement;
+    private Vector2 moveInputs;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +17,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveInputs = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
     private void FixedUpdate()
     {
-        //I should not be programming this late and on this little sleep, what kind of solution is this?
-        rb.AddForce(((transform.forward * (movement.z * moveSpeed)) + (transform.right * (movement.x * moveSpeed)))/(Mathf.Max((movement.x != 0 ? 1 : 0) + (movement.z != 0 ? 1 : 0), 1)));
+
+        Vector3 moveVector = ((transform.forward * moveInputs.y) + (transform.right * moveInputs.x)).normalized;
+
+        rb.AddForce(moveVector * moveSpeed * Time.deltaTime);
     }
 }
