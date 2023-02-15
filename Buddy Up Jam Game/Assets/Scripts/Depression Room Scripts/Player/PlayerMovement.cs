07c10ceb1f1464d172814ffb89da0f2dbf9d7b0e
harrison531 <harrisongardner531@gmail.com>
2023-02-15 11:38:39 -0700

    Enabled movement again and adjusted some camera values

--- Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerMovement.cs
+++ Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerMovement.cs
@@ -11,6 +11,9 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         rb = GetComponent<Rigidbody>();
     }
 
