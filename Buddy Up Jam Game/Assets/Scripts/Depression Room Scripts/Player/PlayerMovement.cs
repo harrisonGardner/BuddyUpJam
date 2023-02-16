649746463af8e789132c85191c0f084932880d32
N6chernin <n6chernin@gmail.com>
2023-02-15 23:14:04 -0500

    Fix of Depression room cam glitch

--- Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerMovement.cs
+++ Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerMovement.cs
@@ -4,31 +4,81 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [Header("Movement")]
+    public float moveSpeed;
 
-    private Rigidbody rb;
-    private Vector2 moveInputs;
-    // Start is called before the first frame update
-    void Start()
+    public float groundDrag;
+
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = true;
-
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MyInput()
     {
-        moveInputs = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 
     private void FixedUpdate()
     {
-        transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
-
-        Vector3 moveVector = ((transform.forward * moveInputs.y) + (transform.right * moveInputs.x)).normalized;
-
-        rb.AddForce(moveVector * moveSpeed * Time.deltaTime);
+        MovePlayer();
     }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
 }
