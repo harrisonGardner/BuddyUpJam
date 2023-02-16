6372ce97bd26cbb19002daf3519b8aba0eb72e29
harrison531 <harrisongardner531@gmail.com>
2023-02-15 22:19:13 -0700

    Revert to cinemachine. Smoothed out camera jitters

--- Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerCam.cs
+++ Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerCam.cs
@@ -32,6 +32,5 @@ public class PlayerCam : MonoBehaviour
         //rotate cam and orientation
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-
     }
 }
