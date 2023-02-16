984d96b047355fb5eed8a716d88ca4778de96f7b
harrison531 <harrisongardner531@gmail.com>
2023-02-15 22:58:06 -0700

    Interactables text popup

--- Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/InteractionText.cs
+++ Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/InteractionText.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InteractionText : MonoBehaviour
 {
     private Vector3 targetPos = Vector3.zero;
+    public float lerpSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class InteractionText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, targetPos, 0.05f);
+        transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed);
     }
 
     public void SetTargetPosition(Vector3 target)
--- Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerInteraction.cs
+++ Buddy Up Jam Game/Assets/Scripts/Depression Room Scripts/Player/PlayerInteraction.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
     private GameObject cam;
-    private GameObject interactionTextCanvas;
+    public GameObject interactionTextCanvas;
+
+    private float interactionTextDefaultScale = 0.01f;
+
+    public float interactDistance = 5f;
 
     public float textOffset = 1f;
+
+    private GameObject lastHighlighted = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,24 +26,40 @@ public class PlayerInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit = new RaycastHit();
+        RaycastHit hit;
 
-        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 50f))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactDistance))
         {
             if (hit.collider.gameObject.CompareTag("Interactable"))
             {
                 interactionTextCanvas.SetActive(true);
-                interactionTextCanvas.GetComponent<InteractionText>().SetTargetPosition(hit.point + (cam.transform.position - interactionTextCanvas.transform.position).normalized * textOffset + new Vector3(0, 0.5f, 0));
+
+                //Snap text to newly highlighted object
+                if (hit.collider.gameObject != lastHighlighted)
+                    interactionTextCanvas.transform.position = hit.point;
+
+                interactionTextCanvas.GetComponent<InteractionText>().SetTargetPosition(hit.point + (cam.transform.position - interactionTextCanvas.transform.position).normalized * textOffset + new Vector3(0, interactionTextCanvas.GetComponent<RectTransform>().sizeDelta.y * (interactionTextCanvas.GetComponent<RectTransform>().localScale.y/2), 0) );
                 interactionTextCanvas.transform.forward = interactionTextCanvas.transform.position - cam.transform.position;
+
+                float scale = interactionTextDefaultScale * (Vector3.Distance(interactionTextCanvas.transform.position, cam.transform.position)/10f);
+                interactionTextCanvas.transform.localScale = new Vector3(scale, scale, scale);
+
+                //First child is main text, second child is sub text
+                interactionTextCanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = hit.collider.gameObject.GetComponent<Interactable>().mainText;
+                interactionTextCanvas.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = hit.collider.gameObject.GetComponent<Interactable>().subText;
+
+                lastHighlighted = hit.collider.gameObject;
             }
             else
             {
                 interactionTextCanvas.SetActive(false);
+                lastHighlighted = null;
             }
         }
         else
         {
             interactionTextCanvas.SetActive(false);
+            lastHighlighted = null;
         }
     }
 }
