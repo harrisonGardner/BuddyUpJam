1bcbbcd144114a5922086c74007746b9490d8ada
harrison531 <harrisongardner531@gmail.com>
2023-02-18 13:16:01 -0700

    Crumble Platforms

--- Buddy Up Jam Game/Assets/Scripts/DreamWorld/WebHazard.cs
+++ Buddy Up Jam Game/Assets/Scripts/DreamWorld/WebHazard.cs
@@ -10,23 +10,35 @@ public class WebHazard : MonoBehaviour
 
     public float spiderMoveSpeed = 5f;
     private GameObject spiderChild;
+    private Vector3 initialPlane;
 
     public void Start()
     {
         spiderChild = transform.GetChild(0).gameObject;
         spiderChild.transform.parent = null;
-        spiderChild.transform.forward = transform.up;
+        spiderChild.transform.up = transform.up;
     }
 
     public void Update()    
     {
         if (targetSet)
         {
-            float angleToTarget = Vector3.SignedAngle(spiderChild.transform.position, target, spiderChild.transform.right);
-            spiderChild.transform.Rotate(new Vector3(0, 0, angleToTarget), Space.Self);
+            spiderChild.transform.right = spiderChild.transform.position - target;
+            //spiderChild.transform.localEulerAngles = new Vector3(90, spiderChild.transform.localEulerAngles.y, spiderChild.transform.localEulerAngles.z);
 
             Vector3 moveTowardsPosition = Vector3.MoveTowards(spiderChild.transform.position, target, spiderMoveSpeed * Time.deltaTime);
+
+            float angleToTarget = Vector3.SignedAngle(spiderChild.transform.position, target, spiderChild.transform.right);
+            //spiderChild.transform.Rotate(new Vector3(0, 0, angleToTarget), Space.Self);
+            //spiderChild.transform.localEulerAngles = new Vector3(initialPlane.x, initialPlane.y, angleToTarget + 90);
+
             spiderChild.transform.position = moveTowardsPosition;
+
+            if (Vector3.Distance(spiderChild.transform.position, target) < 0.5f)
+            {
+                targetSet = false;
+                spiderChild.transform.up = transform.up;
+            }
         }
     }
 
@@ -36,4 +48,7 @@ public class WebHazard : MonoBehaviour
         targetSet = true;
     }
 
+    public void GetRandomPathTarget()
+    { 
+    }
 }
