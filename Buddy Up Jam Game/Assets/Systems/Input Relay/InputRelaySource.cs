01fc8d1813ac020f57b5edd26158f50c14220e1b
N6chernin <n6chernin@gmail.com>
2023-02-18 23:21:02 -0500

    more cam work

--- Buddy Up Jam Game/Assets/Systems/Input Relay/InputRelaySource.cs
+++ Buddy Up Jam Game/Assets/Systems/Input Relay/InputRelaySource.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputRelaySource : MonoBehaviour
 {
     [SerializeField] LayerMask RaycastMask = ~0;
     [SerializeField] float RaycastDistance = 15f;
+    [SerializeField] UnityEvent<Vector2> OnCursorInput = new UnityEvent<Vector2>();
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +18,18 @@ public class InputRelaySource : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //retrvive a ray based on the mouse location
+        // retrieve a ray based on the mouse location
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-
-        //raycast to find what we hit
+        // raycast to find what we have hit
         RaycastHit hitResult;
-        if(Physics.Raycast(mouseRay, out hitResult, RaycastDistance, RaycastMask, QueryTriggerInteraction.Ignore))
+        if (Physics.Raycast(mouseRay, out hitResult, RaycastDistance, RaycastMask, QueryTriggerInteraction.Ignore))
         {
-            //ignore if not us
-            if(hitResult.collider.gameObject != gameObject)
-            {
+            // ignore if not us
+            if (hitResult.collider.gameObject != gameObject)
                 return;
-            }
+
+            OnCursorInput.Invoke(hitResult.textureCoord);
             Debug.Log(hitResult.textureCoord);
         }
     }
