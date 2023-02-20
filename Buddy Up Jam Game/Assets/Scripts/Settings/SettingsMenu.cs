7b49a0f1dcfd7d341ce062e473160ce112c4887b
N6chernin <n6chernin@gmail.com>
2023-02-20 12:02:08 -0500

    options menu

--- Buddy Up Jam Game/Assets/Scripts/Settings/SettingsMenu.cs
+++ Buddy Up Jam Game/Assets/Scripts/Settings/SettingsMenu.cs
@@ -1,11 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+    private int resolutionIndex;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+    }
+
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution (int resolution)
+    {
+        Resolution resolution1 = resolutions[resolutionIndex];
+
+        Screen.SetResolution(resolution1.width, resolution1.height, Screen.fullScreen);
+    }
+
 }
